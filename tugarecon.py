eaabd7adcfcef0fc1f9031a52ddf7c1f5085d246
LordNeoStark <39160972+LordNeoStark@users.noreply.github.com>
2020-01-22 18:23:09 +0000

    Update tugarecon.py

--- tugarecon.py
+++ tugarecon.py
@@ -46,9 +46,6 @@ def parse_args():
     parser.add_argument('-p', '--ports', help='Scan the found subdomains against specified tcp ports')
     parser.add_argument('-v', '--verbose', help='Enable Verbosity and display results in realtime', nargs='?',
                         default=False)
-    parser.add_argument('-t', '--threads', help='Number of threads to use for subbrute bruteforce', type=int,
-                        default=30)
-    parser.add_argument('-e', '--engines', help='Specify a comma-separated list of search engines')
     parser.add_argument('-o', '--output', help='Save the results to text file')
     return parser.parse_args()
 
