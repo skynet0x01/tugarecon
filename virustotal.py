e65bf923189c286daa2c1aad6b701343d8cc65fe
LordNeoStark <39160972+LordNeoStark@users.noreply.github.com>
2020-01-23 13:41:10 +0000

    Add files via upload

---
+++ virustotal.py
@@ -0,0 +1,47 @@
+# TugaRecon - virustotal, write by LordNeoStark
+# import modules
+import time
+import requests
+
+from functions import useragent
+from functions import write_file
+
+
+class Virustotal:
+
+    def __init__(self, target, output):
+
+        subdomains = set()
+        count = 0
+        subdomainscount = 0
+        start_time = time.time()
+        self.module_name = "VirusTotal"
+        self.engine = "virustotal"
+
+        print(f"VirusTotal: Enumerating subdomains now for {target} \n")
+
+        url = f'https://www.virustotal.com/ui/domains/{target}/subdomains?limit=40'
+
+        try:
+            response = requests.get(url, headers={'User-Agent': useragent()})
+
+            while subdomainscount < 40:
+                subdomains = response.json()["data"][subdomainscount]["id"]
+                subdomainscount = subdomainscount + 1
+                count = count + 1
+                print(f"[*] {subdomains}")
+
+                if output is not None:
+                    write_file(subdomains, self.engine +output)
+
+            if output:
+                print(f"\nSaving result... {self.engine +output}")
+
+            print(f"\n[**] TugaRecon is complete. VirusTotal: {count} subdomains have been found in %s seconds" % (
+                    time.time() - start_time))
+
+            if not subdomains:
+                print(f"[x] No data found for {target} using VirusTotal.")
+
+        except ValueError:
+            pass
