fdebbd0121f6cb514096698cb63be945d01ada7a
LordNeoStark <39160972+LordNeoStark@users.noreply.github.com>
2020-01-23 13:34:18 +0000

    Add files via upload

--- tugarecon.py
+++ tugarecon.py
@@ -44,11 +44,6 @@ def parse_args():
     parser.add_argument('-d', '--domain', type=str, help="Domain name to enumerate it's subdomains", required=True)
     parser.add_argument('-b', '--bruteforce', help='Enable the subbrute bruteforce module', nargs='?', default=False)
     parser.add_argument('-p', '--ports', help='Scan the found subdomains against specified tcp ports')
-    parser.add_argument('-v', '--verbose', help='Enable Verbosity and display results in realtime', nargs='?',
-                        default=False)
-    parser.add_argument('-t', '--threads', help='Number of threads to use for subbrute bruteforce', type=int,
-                        default=30)
-    parser.add_argument('-e', '--engines', help='Specify a comma-separated list of search engines')
     parser.add_argument('-o', '--output', help='Save the results to text file')
     return parser.parse_args()
 
