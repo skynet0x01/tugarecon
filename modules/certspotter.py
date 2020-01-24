593e9268d8dc8d8704b97ac24785eda0ec18e5c0
LordNeoStark <39160972+LordNeoStark@users.noreply.github.com>
2020-01-24 15:42:31 +0000

    Add files via upload

--- modules/certspotter.py
+++ modules/certspotter.py
@@ -5,22 +5,30 @@ import requests
 
 from functions import useragent
 from functions import write_file
-from functions import G,W
+from functions import G, W
 
 class Certspotter:
 
     def __init__(self, target, output):
 
-        subdomains = set()
-        count = 0
-        subdomainscount = 0
-        start_time = time.time()
+        self.target = target
+        self.output = output
         self.module_name = "CertSpotter"
         self.engine = "certspotter"
 
         print(G + f"CertSpotter: Enumerating subdomains now for {target} \n" + W)
 
-        url = f'https://api.certspotter.com/v1/issuances?domain={target}&include_subdomains=true&expand=dns_names'
+        url = self.subdomains_list()
+        self.enumerate(url, output)
+
+    def subdomains_list(self):
+        url = f'https://api.certspotter.com/v1/issuances?domain={self.target}&include_subdomains=true&expand=dns_names'
+        return url
+
+    def enumerate(self, url, output):
+        subdomains = set()
+        subdomainscount = 0
+        start_time = time.time()
 
         try:
             response = requests.get(url, headers={'User-Agent': useragent()})
@@ -28,20 +36,19 @@ class Certspotter:
             while subdomainscount < 100:
                 subdomains = response.json()[subdomainscount]["dns_names"][0]
                 subdomainscount = subdomainscount + 1
-                count = count + 1
                 print(f"[*] {subdomains}")
 
-                if output is not None:
-                    write_file(subdomains, self.engine +output)
+                if self.output is not None:
+                    write_file(subdomains, self.engine + self.output)
 
-            if output:
-                print(f"\nSaving result... {self.engine +output}")
+            if self.output:
+                print(f"\nSaving result... {self.engine + self.output}")
 
-            print(G + f"\n[**] TugaRecon is complete. CertSpotter: {count} subdomains have been found in %s seconds" % (
+        except IndexError:
+            pass
+
+        print(G + f"\n[**] TugaRecon is complete. CertSpotter: {subdomainscount} subdomains have been found in %s seconds" % (
                     time.time() - start_time) + W)
 
-            if not subdomains:
-                print(f"[x] No data found for {target} using CertSpotter.")
-
-        except ValueError:
-            pass
+        if not subdomains:
+            print(f"[x] No data found for {target} using CertSpotter.")
