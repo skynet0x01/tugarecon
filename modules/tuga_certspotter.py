99ea663ae58e0289bd0efa672b78bd75f18eb704
skynet0x01 <39160972+skynet0x01@users.noreply.github.com>
2021-10-31 15:56:45 +0000

    Add files via upload

---
+++ modules/tuga_certspotter.py
@@ -0,0 +1,60 @@
+# TugaRecon - certspotter, write by skynet0x01
+# TugaRecon, tribute to Portuguese explorers reminding glorious past of this country
+# Bug Bounty Recon, search for subdomains and save in to a file
+# Coded By skynet0x01
+
+# import modules
+import time
+import requests
+# Import internal modules
+from modules import tuga_useragents #random user-agent
+# Import internal functions
+from functions import write_file
+from functions import DeleteDuplicate
+from functions import G, W
+################################################################################
+class Certspotter:
+
+    def __init__(self, target, output):
+
+        self.target = target
+        self.output = output
+        self.module_name = "CertSpotter"
+        self.engine = "certspotter"
+
+        print(G + f"CertSpotter: Enumerating subdomains now for {target} \n" + W)
+
+        self.response = self.engine_url()
+        self.enumerate(self.response, output, target)
+        if self.output is not None:
+            DeleteDuplicate(self.engine + '_' + self.output, target)
+################################################################################
+    def engine_url(self):
+        try:
+            url = f'https://api.certspotter.com/v1/issuances?domain={self.target}&include_subdomains=true&expand=dns_names'
+            response = requests.get(url, headers=tuga_useragents.useragent())
+        except requests.exceptions.Timeout:
+            pass
+        return response
+################################################################################
+    def enumerate(self, response, output, target):
+        subdomains = []
+        subdomainscount = 0
+        start_time = time.time()
+        try:
+            while subdomainscount < 100:
+                subdomains = response.json()[subdomainscount]["dns_names"][0]
+                subdomainscount = subdomainscount + 1
+                print(f"[*] {subdomains}")
+
+                if self.output is not None:
+                    write_file(subdomains, self.engine + '_' + self.output, target)
+            if self.output:
+                print(f"\nSaving result... {self.engine + '_' + self.output}")
+        except:
+            pass
+        if not subdomains:
+            print(f"[x] No data found for {self.target} using CertSpotter.")
+        else:
+            print(G + f"\n[**] TugaRecon is complete. CertSpotter: {subdomainscount} subdomains have been found in %s seconds" % (
+                    time.time() - start_time) + W)
---
+++ modules/tuga_crt.py
@@ -0,0 +1,71 @@
+# TugaRecon - crt module, write by skynet0x01
+# TugaRecon, tribute to Portuguese explorers reminding glorious past of this country
+# Bug Bounty Recon, search for subdomains and save in to a file
+# Coded By skynet0x01
+# import modules
+import time
+import requests
+# Import internal modules
+from modules import tuga_useragents #random user-agent
+# Import internal functions
+from functions import write_file
+from functions import DeleteDuplicate
+from functions import G, W
+################################################################################
+class CRT:
+
+    def __init__(self, target, output):
+
+        self.target = target
+        self.output = output
+        self.module_name = "SSL Certificates"
+        self.engine = "crt"
+
+        print(G + f"SSL Certificates: Enumerating subdomains now for {target} \n" + W)
+
+        self.response = self.engine_url()
+        self.enumerate(self.response, output, target)
+        if self.output is not None and self.subdomainscount != 0:
+            DeleteDuplicate(self.engine + '_' + self.output, target)
+        else:
+            pass
+################################################################################
+    def engine_url(self):
+        try:
+            url = f"https://crt.sh/?q={self.target}&output=json"
+            response = requests.get(url, headers=tuga_useragents.useragent())
+            return response
+        except requests.exceptions.Timeout:
+            pass
+################################################################################
+    def enumerate(self, response, output, target):
+        subdomains = set()
+        self.subdomainscount = 0
+        start_time = time.time()
+
+        try:
+            while self.subdomainscount < 10000:
+                subdomains = response.json()[self.subdomainscount]["name_value"]
+
+                if not subdomains:
+                    print(f"[x] Oops! No data found for {self.target} using  SSL Certificates.")
+                else:
+                    self.subdomainscount = self.subdomainscount + 1
+                    if "@" in subdomains:  # filter for emails
+                        pass
+                    else:
+                        print(f"[*] {subdomains}")
+
+                        if self.output is not None:
+                            write_file(subdomains, self.engine + '_' + self.output, target)
+            if self.output:
+                print(f"\nSaving result... {self.engine + '_'+ self.output}")
+        except IndexError:
+            pass
+
+        #if not subdomains:
+            #print(f"[x] Oops! No data found for {self.target} using  SSL Certificates.")
+        #else:
+            print(
+                G + f"\n[**] TugaRecon is complete.  SSL Certificates: {self.subdomainscount} subdomains have been found in %s seconds" % (
+                        time.time() - start_time) + W)
