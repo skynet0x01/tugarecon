3ef6e1ecdf87c42bab7a2747c42687693f60ff72
skynet0x01 <39160972+skynet0x01@users.noreply.github.com>
2022-04-04 20:40:18 +0100

    Add files via upload

---
+++ utils/tuga_results.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# TugaRecon, tribute to Portuguese explorers reminding glorious past of this country
+# Bug Bounty Recon, search for subdomains and save in to a file
+# Coded By skynet0x01
+################################################################################
+import os
+from utils.tuga_colors import G, Y, B, R, W
+
+def main_work_subdirs():
+    rootdir = "results"
+    print("\nResults: ")
+    print(G + "**************************************************************\n" + W)
+    for root, dirs, files in os.walk(rootdir):
+        dirs.sort()
+        if root == rootdir:
+            for domain in dirs:
+                print(Y + f"[Domain]", domain  + W)
