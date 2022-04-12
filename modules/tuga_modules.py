1ecdaf9dd28cb05ccaab267c38af50f35f066a3d
skynet0x01 <39160972+skynet0x01@users.noreply.github.com>
2022-04-12 10:09:39 +0100

    Add files via upload

--- modules/tuga_modules.py
+++ modules/tuga_modules.py
@@ -1,3 +1,5 @@
+import time
+
 # Import internal modules
 from modules import tuga_certspotter
 from modules import tuga_crt
@@ -6,3 +8,18 @@ from modules import tuga_threatcrowd
 from modules import tuga_alienvault
 from modules import tuga_threatminer
 from modules import tuga_omnisint
+from utils.tuga_colors import G, Y, B, R, W
+################################################################################
+# Run all Modules...
+def queries(target):
+    print(G + "Enumerating subdomains for " + target + " ...\n" + W)
+    time.sleep(1)
+    print(R + "[-] Searching " + target + " in CertsPotter " + W)
+    print(R + "[-] Searching " + target + " in SSL Certificates " + W)
+    print(R + "[-] Searching "  + target + " in HackerTarget " + W)
+    print(R + "[-] Searching "  + target + " in ThreatCrowd " + W)
+    print(R + "[-] Searching "  + target + " in Alienvault " + W)
+    print(R + "[-] Searching "  + target + " in Threatminer" + W)
+    print(R + "[-] Searching "  + target + " in Omnisint\n" + W)
+    #time.sleep(0.5)
+    return (0)
