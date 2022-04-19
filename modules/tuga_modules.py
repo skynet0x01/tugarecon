49b9d8f47fc5b43c87f6146e4bf7e3c01d3020af
skynet0x01 <39160972+skynet0x01@users.noreply.github.com>
2022-04-19 14:57:24 +0100

    Add files via upload

--- modules/tuga_modules.py
+++ modules/tuga_modules.py
@@ -1,5 +1,9 @@
+# TugaRecon - crt module, write by skynet0x01
+# TugaRecon, tribute to Portuguese explorers reminding glorious past of this country
+# Bug Bounty Recon, search for subdomains and save in to a file
+# Coded By skynet0x01
+# import modules
 import time
-
 # Import internal modules
 from modules import tuga_certspotter
 from modules import tuga_crt
@@ -8,6 +12,7 @@ from modules import tuga_threatcrowd
 from modules import tuga_alienvault
 from modules import tuga_threatminer
 from modules import tuga_omnisint
+from modules import tuga_sublist3r
 from utils.tuga_colors import G, Y, B, R, W
 ################################################################################
 # Run all Modules...
@@ -20,6 +25,7 @@ def queries(target):
     print(R + "[-] Searching "  + target + " in ThreatCrowd " + W)
     print(R + "[-] Searching "  + target + " in Alienvault " + W)
     print(R + "[-] Searching "  + target + " in Threatminer" + W)
-    print(R + "[-] Searching "  + target + " in Omnisint\n" + W)
+    print(R + "[-] Searching "  + target + " in Omnisint" + W)
+    print(R + "[-] Searching "  + target + " in API Sublist3r\n" + W)
     #time.sleep(0.5)
     return (0)
---
+++ modules/tuga_sublist3r.py
@@ -0,0 +1,59 @@
+# TugaRecon - crt module, write by skynet0x01
+# TugaRecon, tribute to Portuguese explorers reminding glorious past of this country
+# Bug Bounty Recon, search for subdomains and save in to a file
+# Coded By skynet0x01
+# import modules
+import time
+import requests
+import json
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Import internal modules
+from modules import tuga_useragents #random user-agent
+# Import internal functions
+from utils.tuga_functions import write_file
+from utils.tuga_functions import DeleteDuplicate
+from utils.tuga_colors import G, Y, B, R, W
+################################################################################
+class Sublist3r:
+
+    def __init__(self, target):
+
+        self.target = target
+        self.module_name = "Sublist3r"
+        self.engine = "sublist3r"
+        self.response = self.engine_url() # URL
+
+        if self.response != 1:
+            self.enumerate(self.response, target) # Call the function enumerate
+        else:
+            pass
+################################################################################
+    def engine_url(self):
+        try:
+            response = requests.get(f'https://api.sublist3r.com/search.php?domain={self.target}').text
+            return response
+        except requests.ConnectionError:
+            response = 1
+            return response
+################################################################################
+    def enumerate(self, response, target):
+        subdomains = []
+        self.subdomainscount = 0
+        start_time = time.time()
+        #################################
+        try:
+            extract_sub = json.loads(response)
+            #print(extract_sub)
+            for i in extract_sub:
+                self.subdomainscount = self.subdomainscount + 1
+                #subdomains = response.json()[self.subdomainscount]["name_value"]
+                subdomains = i
+                #print(f"{subdomains}")
+
+                write_file(subdomains, target)
+        except Exception as e:
+            pass
+                #################################
