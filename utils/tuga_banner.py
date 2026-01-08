60fe24c42132afc7ea89993096955893a1628aed
Skynet0x01 <skynet0x01@protonmail.com>
2026-01-08 19:28:24 +0000

    Version 2.5

--- utils/tuga_banner.py
+++ utils/tuga_banner.py
@@ -42,5 +42,5 @@ def banner():
             /_/  \__,_/\__, /\__,_/_/ |_|\___/\___/\____/_/ /_/  Version 2.5
                       /____/                               # Coded By skynet0x01 #
     """ + W)
-    print(Y + "TugaRecon, tribute to Portuguese explorers reminding glorious past of this country. 2020-2025\n" + W)
+    print(Y + "TugaRecon, tribute to Portuguese explorers reminding glorious past of this country. 2020-2026\n" + W)
 
